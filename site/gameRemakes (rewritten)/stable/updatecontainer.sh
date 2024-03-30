4fa15d060f3f7c53e11cab0a8dd6fe638b2f2bd0
Gridloch <margaretwalker999@gmail.com>
2024-03-30 11:20:05 +0000

    Updated stable submodule

--- site/gameRemakes (rewritten)/stable/updatecontainer.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/bash
-
-docker stop dockersite-container
-docker rm dockersite-container  
-docker build -t dockersite .
-docker run -dit --name dockersite-container -p 8080:80 dockersite
-
-echo "updated container"
